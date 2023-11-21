b7f2fc29eff73e01d54510f1d87979542c1e50ca
xuzuodong <hi@xzd.me>
2023-11-21 17:31:49 +0800

    feat: 默认使用 gpt-4-turbo

--- app/constant.ts
+++ app/constant.ts
@@ -68,6 +68,10 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const DEFAULT_MODELS = [
   {
+    name: "gpt-4-1106-preview",
+    available: true,
+  },
+  {
     name: "gpt-4",
     available: true,
   },
