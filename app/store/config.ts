0327fe249b3418f6c51f666ea4a014523a90750f
xuzuodong <hi@xzd.me>
2024-09-06 11:41:56 +0800

    chore: 模型调整为 4o-mini

--- app/store/config.ts
+++ app/store/config.ts
@@ -39,7 +39,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-4-1106-preview" as ModelType,
+    model: "gpt-4o-mini-2024-07-18" as ModelType,
     // model: 'gpt-3.5-turbo' as ModelType,
     temperature: 0.5,
     top_p: 1,
